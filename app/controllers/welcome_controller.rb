6dc37882729866eaf5a5265070f64e7767610731
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-30 10:41:28 -0600

    Adds logic for valid or invalid words'

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,19 @@ class WelcomeController < ApplicationController
   end
 
   def new
-    Validation.new(params[:q])
+    @word = params[:q]
+    conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/inflections/en/#{@word}")
+    @resp = conn.get do |conn|
+      conn.headers['app_id'] = ENV['OXFORD_ID']
+      conn.headers['app_key'] = ENV['SCRABBLE_KEY']
+    end
+    @body = JSON.parse(@resp.body)
+
+    if @body['results']
+      @message = "'#{@body['results'][0]['id']}' is a valid word and its root form is '#{@body['results'][0]["lexicalEntries"][0]['inflectionOf'][0]['text']}'."
+    else
+      @message = "'foxez' is not a valid word."
+    end
+    binding.pry
   end
 end
--- app/services/validation.rb
+++ app/services/validation.rb
@@ -6,10 +6,9 @@ class Validation
 
   def validates_word
     conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/inflections/en/#{@word}")
-    resp = conn.get do |conn|
+    @resp = conn.get do |conn|
       conn.headers['app_id'] = ENV['OXFORD_ID']
       conn.headers['app_key'] = ENV['SCRABBLE_KEY']
     end
-    binding.pry
   end
 end
